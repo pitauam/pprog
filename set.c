4e26a0358ffdc2ba3aa29421f48a9bbfbe8f8313
pitauam <santiago.pita@estudiante.uam.es>
2026-02-23 10:27:15 +0100

    corregido _set

--- set.c
+++ set.c
@@ -10,13 +10,31 @@
 #include "set.h"
 #define SIZE 256 /*64 Ids */
 
-struct Set
+struct _Set
 {
     Id ids[SIZE];   /*Array of static Ids*/
     int n_ids;      /*Keeps track of the number of ids in the Set*/
 };
 
-Set* set_create();
+Set* set_create()
+{
+    Set *new_set = NULL;
+    int i;
+    
+    new_set = (Set*)calloc(1,sizeof(Set));
+    if (!new_set) {return NULL;}
+
+    new_set->n_ids = 0;
+    for (i = 0; i < SIZE; i++)
+    {
+        new_set->ids[i] = NO_ID;
+    }
+
+
+
+
+    return new_set;
+}
 
 Status set_destroy(Set* set);
 
