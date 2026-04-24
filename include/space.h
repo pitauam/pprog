932c7dca7d51ffe467dc67044ac515fddb306c70
paula6556 <polillas1976@gmail.com>
2026-04-24 21:26:52 +0200

    space unificado (con discovered y characters)

--- include/space.h
+++ include/space.h
@@ -2,9 +2,9 @@
  * @brief It defines the space module interface
  *
  * @file space.h
- * @author Profesores PPROG
- * @version 2
- * @date 24-02-2025
+ * @author Samuel Manzorro, Paula de la Fuente, Santiago Pita, Mario Rodriguez
+ * @version 4
+ * @date 27-01-2025
  * @copyright GNU Public License
  */
 
@@ -12,13 +12,21 @@
 #define SPACE_H
 
 #include "types.h"
+#include "object.h"
 #include "set.h"
+#include "character.h"
+
+
+#define GDESC_WIDTH (9+1) /*+1 to store the '\0' character*/
+#define GDESC_HEIGHT 5
+
+
 
 typedef struct _Space Space;
 
 /**
  * @brief It creates a new space, allocating memory and initializing its members
- * @author Profesores PPROG
+ * @author Samuel Manzorro
  *
  * @param id the identification number for the new space
  * @return a new space, initialized
@@ -27,7 +35,7 @@ Space* space_create(Id id);
 
 /**
  * @brief It destroys a space, freeing the allocated memory
- * @author Profesores PPROG
+ * @author Samuel Manzorro
  *
  * @param space a pointer to the space that must be destroyed
  * @return OK, if everything goes well or ERROR if there was some mistake
@@ -36,7 +44,7 @@ Status space_destroy(Space* space);
 
 /**
  * @brief It gets the id of a space
- * @author Profesores PPROG
+ * @author Samuel Manzorro
  *
  * @param space a pointer to the space
  * @return the id of space
@@ -45,7 +53,7 @@ Id space_get_id(Space* space);
 
 /**
  * @brief It sets the name of a space
- * @author Profesores PPROG
+ * @author Samuel Manzorro
  *
  * @param space a pointer to the space
  * @param name a string with the name to store
@@ -55,100 +63,12 @@ Status space_set_name(Space* space, char* name);
 
 /**
  * @brief It gets the name of a space
- * @author Profesores PPROG
+ * @author Samuel Manzorro
  *
  * @param space a pointer to the space
  * @return  a string with the name of the space
  */
 const char* space_get_name(Space* space);
-
-/**
- * @brief It adds a object to the space
- * @author Mario Rodriguez
- *
- * @param space a pointer to the space
- * @param id an Id, specifying if in the space there is an object (Id) or not (NO_ID)
- * @return OK, if everything goes well or ERROR if there was some mistake
- */
-Status space_add_object(Space* space, Id id);
-
-/**
- * @brief It adds a character to the space
- * @author Santiago Pita
- *
- * @param space a pointer to the space
- * @param id an Id, specifying if in the space there is a character (Id) or not (NO_ID)
- * @return OK, if everything goes well or ERROR if there was some mistake
- */
-Status space_add_character(Space* space, Id id);
-
-/**
- * @brief It removes an object from the space
- * @author Mario Rodriguez
- *
- * @param space a pointer to the space
- * @param id Id of the object that will be deleted
- * @return OK, if everything goes well or ERROR if there was some mistake
- */
-Status space_remove_object(Space* space, Id id);
-
-/**
- * @brief It removes a character from the space
- * @author Santiago Pita
- *
- * @param space a pointer to the space
- * @param id Id of the character that will be deleted
- * @return OK, if everything goes well or ERROR if there was some mistake
- */
-Status space_remove_character(Space* space, Id id);
-
-/**
- * @brief It finds the id of an object in a space
- * @author Mario Rodriguez
- *
- * @param space a pointer to the space
- * @param id id of the object you want to find
- * @return True if it finds the object, otherwise False
- */
-Bool space_find_object(Space* space, Id id);
-
-/**
- * @brief It finds the id of a character in a space
- * @author Santiago Pita
- *
- * @param space a pointer to the space
- * @param id id of the character to find
- * @return True if it finds the character, otherwise False
- */
-Bool space_find_character(Space* space, Id id);
-
-/**
- * @brief It gets the number of the objects of the space
- * @author Mario Rodriguez
- *
- * @param space a pointer to the space
- * @return an int with the number of objects in the space
- */
-int space_get_n_objects(Space* space);
-
-/**
- * @brief It gets the number of the characters of the space
- * @author Santiago Pita
- *
- * @param space a pointer to the space
- * @return an int with the number of characters in the space
- */
-int space_get_n_characters(Space* space);
-
-/**
- * @brief It prints the space information
- * @author Profesores PPROG
- *
- * This fucntion shows the id and name of the space, the spaces that surrounds it and wheter it has an object or not.
- * @param space a pointer to the space
- * @return OK, if everything goes well or ERROR if there was some mistake
- */
-Status space_print(Space* space);
 
 /**
  * @brief It sets the graphic description of a space
@@ -161,6 +81,74 @@ Status space_print(Space* space);
 Status space_set_gdesc(Space *space, char *gdesc);
 
 /**
+ * @brief It tells if the space has an object or not
+ * @author Samuel Manzorro
+ * 
+ * @param space a pointer to the space
+ * @param object_id the id of the object to be found
+ * @return TRUE, if the object is in the space, FALSE in other cases
+ */
+Bool space_find_object(Space* space, Id object_id);
+
+/**
+ * @brief It adds an object to the space
+ * @author Samuel Manzorro
+ * 
+ * @param space a pointer to the space
+ * @param object_id the id of the object to be added
+ * @return OK, if everything goes well or ERROR if there was some mistake
+ */
+Status space_add_object(Space* space, Id object_id);
+
+/**
+ * @brief It deletes an object from the space
+ * @author Samuel Manzorro
+ * 
+ * @param space a pointer to the space
+ * @param object_id the id of the object to be deleted
+ * @return OK, if everything goes well or ERROR if there was some mistake
+ */
+Status space_remove_object(Space* space, Id object_id);
+
+
+/**
+ * @brief It gets the set of objects in the space
+ * @author Samuel Manzorro
+ * 
+ * @param space a pointer to the space
+ * @return the set of objects in the space
+ */
+Set *space_get_objects(Space *space);
+
+/**
+ * @brief It sets a space to be discovered or not
+ * @author Paula de la Fuente
+ *
+ * @param space a pointer to the space
+ * @param discovered whether the space is discovered or not
+ * @return OK, if everything goes well or ERROR if there was some mistake
+ */
+Status space_set_discovered(Space* space, Bool discovered);
+
+/**
+ * @brief It gets whether a space is discovered or not
+ * @author Paula de la Fuente
+ *
+ * @param space a pointer to the space
+ * @return TRUE if the space is discovered, FALSE if not
+ */
+Bool space_is_discovered(Space* space);
+
+/**
+ * @brief It gets the number of the objects of the space
+ * @author Mario Rodriguez
+ *
+ * @param space a pointer to the space
+ * @return an int with the number of objects in the space
+ */
+int space_get_n_objects(Space* space);
+
+/**
  * @brief It gets the graphic description of a space
  * @author Santiago Pita
  *
@@ -169,5 +157,54 @@ Status space_set_gdesc(Space *space, char *gdesc);
  * @return graphic description, or NULL if there isn't one
  */
 char* space_get_gdesc_line(Space *space, int line);
+
+
+/**
+ * @brief It adds a character to the space
+ * @author Santiago Pita
+ *
+ * @param space a pointer to the space
+ * @param id an Id, specifying if in the space there is a character (Id) or not (NO_ID)
+ * @return OK, if everything goes well or ERROR if there was some mistake
+ */
+Status space_add_character(Space* space, Id id);
+
+/**
+ * @brief It removes a character from the space
+ * @author Santiago Pita
+ *
+ * @param space a pointer to the space
+ * @param id Id of the character that will be deleted
+ * @return OK, if everything goes well or ERROR if there was some mistake
+ */
+Status space_remove_character(Space* space, Id id);
+
+/**
+ * @brief It finds the id of a character in a space
+ * @author Santiago Pita
+ *
+ * @param space a pointer to the space
+ * @param id id of the character to find
+ * @return True if it finds the character, otherwise False
+ */
+Bool space_find_character(Space* space, Id id);
+
+/**
+ * @brief It gets the number of the characters of the space
+ * @author Santiago Pita
+ *
+ * @param space a pointer to the space
+ * @return an int with the number of characters in the space
+ */
+int space_get_n_characters(Space* space);
+
+/**
+ * @brief It prints the space information
+ * @author Samuel Manzorro
+ *
+ * @param space a pointer to the space
+ * @return OK, if everything goes well or ERROR if there was some mistake
+ */
+Status space_print(Space* space);
 
 #endif
