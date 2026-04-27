a405ce56eca6b298e2927950b0bcf47e65efa87f
elhereu <el.hereu204@gmail.com>
2026-04-27 11:20:51 +0200

    Add files via upload
    Direcciones up y down actualizadas (faltaría añadir al castle.dat original el nuevo mapa actualizado con ups y downs)

---
+++ types.h
@@ -0,0 +1,25 @@
+/**
+ * @brief It defines common types for the whole project
+ *
+ * @file types.h
+ * @author Profesores PPROG
+ * @version 0
+ * @date 27-01-2025
+ * @copyright GNU Public License
+ */
+
+#ifndef TYPES_H
+#define TYPES_H
+
+#define WORD_SIZE 1000 /*!< size of the words*/
+#define NO_ID -1       /*!< used when an ID does not exist*/
+
+typedef long Id;
+
+typedef enum { FALSE, TRUE } Bool;          /*!< whether it is true or false, used when returning booleans in a function*/
+
+typedef enum { ERROR, OK } Status;          /*!< whether a function goes well or has an error*/
+
+typedef enum { NO_DIR = -1, N, S, E, W, U, D} Direction;      /*!< whether a space is connected north, south, east, west, up or down or there is no connection*/
+
+#endif
