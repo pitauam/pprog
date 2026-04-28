0df5aeee0c130c78ace980a7bfd8287c2db70e4c
pitauam <232541784+pitauam@users.noreply.github.com>
2026-04-28 12:55:40 +0200

    fixed set glitch when deleting element in set

---
+++ include/game_rules.h
@@ -0,0 +1,29 @@
+/**
+ * @brief It defines the gamerules module
+ *
+ * @file game_rules.h
+ * @version 0
+ * @date 28-04-2026
+ */
+
+ #ifndef GAME_RULES_H
+ #define GAME_RULES_H
+
+ #include "types.h"
+ /*
+#define CMD_LENGTH 30 /*!< Maximum lenght of a command input 
+#define MAX_CMD_ARG 3 /*!< Maximum arguments a command can have 
+#define N_CMDT 2
+#define N_CMD 12
+
+typedef enum { CMDS, CMDL } CommandType;
+
+typedef enum { NO_CMD = -1, UNKNOWN, EXIT, MOVE, TAKE, DROP, ATTACK, CHAT, INSPECT, RECRUIT, ABANDON, USE } CommandCode;
+
+typedef struct _Command Command;
+
+
+Command* command_create();
+*/
+
+#endif
